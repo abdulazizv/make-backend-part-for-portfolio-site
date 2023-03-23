c0938fdc99362509dc27549b10b762803e3c412c
abdulazizv <abdulazizheir@gmail.com>
2023-03-23 13:10:11 +0500

    added new changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath:'.env',isGlobal: true}), UserModule],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
