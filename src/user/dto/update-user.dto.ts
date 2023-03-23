f25e4fdfba281fbc9d05c0c7bf5d71a1bc26bcb5
abdulazizv <abdulazizheir@gmail.com>
2023-03-23 12:03:29 +0500

    added methods

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString,IsDate } from 'class-validator';
+import { IsOptional } from 'class-validator/types/decorator/decorators';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    @IsString()
+    readonly email: string;
+    @IsOptional()
+    @IsString()
+    readonly password: string;
+    @IsOptional()
+    @IsString()
+    readonly full_name: string;
+    @IsOptional()
+    @IsString()
+    readonly phone_number: string;
+    @IsOptional()
+    @IsString()
+    readonly cv_link: string;
+    @IsOptional()
+    @IsDate()
+    readonly birth_date: Date;
+    @IsOptional()
+    @IsString()
+    readonly address: string;
+    photo: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,HttpException,HttpStatus } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = await this.prismaService.user.create({
+      data:{
+      email: createUserDto.email,
+      password: createUserDto.password,
+      full_name: createUserDto.full_name,
+      phone_number: createUserDto.phone_number,
+      cv_link: createUserDto.cv_link,
+      birth_date: createUserDto.birth_date,
+      address: createUserDto.address,
+      photo: createUserDto.photo,
+      }
+    });
+    if(newUser) {
+      return newUser;
+    }
+    throw new HttpException(
+      'Error has been detected during save information',
+       HttpStatus.BAD_GATEWAY
+    )
+
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User[]> {
+    const allUsers = await this.prismaService.user.findMany();
+    if(allUsers.length >= 1)
+      return allUsers;
+    throw new HttpException(
+      'Database not found',
+      HttpStatus.NOT_FOUND
+    )
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.prismaService.user.update({
+      where:{id},
+      data:{
+        ...updateUserDto
+      }
+    });
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const deletedUser = await this.prismaService.user.delete({
+      where:{id}
+    })
+    return deletedUser;
   }
 }
