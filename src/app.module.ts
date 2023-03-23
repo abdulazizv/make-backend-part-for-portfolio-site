d1f2f0b683c070c991c349f1cb69e7a71e44f4a6
abdulazizv <abdulazizheir@gmail.com>
2023-03-23 11:10:29 +0500

    added collections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({envFilePath:'.env',isGlobal: true}), UserModule],
   controllers: [],
   providers: [],
 })
